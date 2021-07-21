5773479dec66a0f6e0ab7913ce8a30b00d81e146
mariangace <mariangace@gmail.com>
2021-07-21 14:30:30 -0500

    save packages version

---
+++ click-counter/src/App.test.js
@@ -0,0 +1,19 @@
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import App from './App';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment button', () => {});
+
+test('renders counter display', () => {});
+
+test('counter display start at 0', () => {});
+
+test('clicking button increments counter', () => {});
